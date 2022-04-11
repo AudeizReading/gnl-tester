c53c09fe045ee9b261e1165446ffc291257e7b64
alellouc <alellouc@student.42nice.fr>
2022-04-11 08:11:19 +0200

    update tester about new subject

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 10:23:24 by alellouc          #+#    #+#             */
-/*   Updated: 2021/06/04 09:46:52 by alellouc         ###   ########.fr       */
+/*   Updated: 2022/04/11 08:10:58 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,47 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-void	ft_putstr_fd(char *str, int fd);
+size_t	ft_strlen(char *s);
+void	ft_putstr_fd(char *str, int fd, int toggle);
 
-void	ft_putstr_fd(char *str, int fd)
+size_t	ft_strlen(char *s)
 {
-	while (*str)
-		write(fd, str++, 1);
+	size_t	len = 0;
+	while (s[len])
+		len++;
+	return len;
+}
+
+void	ft_putstr_fd(char *str, int fd, int toggle)
+{
+	if (str)
+		write(fd, str, ft_strlen(str));
+	if (toggle)
+		write(fd, "\n", 1);
 }
 
 int	main(int argc, char **argv)
 {
 	char	*line;
 	size_t	fd;
-	int		gnl;
+//	int		gnl;
 
 	if (argc == 2)
 		fd = open(argv[1], O_RDONLY);
 	else
 		fd = 0;
-	while ((gnl = get_next_line(fd, &line)) > 0)
+	// subject has changed since
+	while ((line = get_next_line(fd)))
+	{
+		ft_putstr_fd(fd, line, 1);
+		free(line);
+	}
+	if (line)
+	{
+		ft_putstr_fd(fd, line, 1);
+		free(line);
+	}
+/*	while ((gnl = get_next_line(fd, &line)) > 0)
 	{
 		ft_putstr_fd(line, 1);
 		ft_putstr_fd("\n", 1);
@@ -45,7 +67,7 @@ int	main(int argc, char **argv)
 		ft_putstr_fd(line, 1);
 		ft_putstr_fd("\n", 1);
 		free(line);
-	}
+	}*/
 	close(fd);
 	return (0);
 }
